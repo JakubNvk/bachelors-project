3bcf13235dd01cfece209c86654c36bfe3d140e1
Jakub Novak <jakub.novak@gmail.com>
2016-02-20 19:38:06 +0100

    removed redundant html
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- stops/get_stops.py
+++ stops/get_stops.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from parse import *
 from parse import compile
 
-soup = BeautifulSoup(open("stops.html"))
+soup = BeautifulSoup(open("stops.html")) # https://tools.wmflabs.org/query2map/index.php?BBOX=16.953106,48.105253,17.244244,48.219984&key=public_transport&value=stop_position&types=points-lines-areas-infos
 l = compile("https://tools.wmflabs.org/query2map/queryinmap.php?BBOX={}&name={}&key=public_transport&value=stop_position&types=points-lines-areas-infos")
 for link in soup.find_all('a'):
     parsed_link = l.parse(link.get('href'))
