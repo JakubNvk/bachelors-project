a1d679f57e90c548aa153406d178394bb0aafd90
Jakub Novak <jakub.novak@gmail.com>
2016-04-09 18:17:40 +0200

    finished get_nearest_stop function
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- bcf.py
+++ bcf.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import imhdsk
-from trams import trams
+from trams import trams, trams_rev
 from mapbox import Directions
 from geopy.geocoders import Nominatim
 from geopy.distance import vincenty
@@ -9,32 +9,38 @@ from geopy.distance import vincenty
 MAPBOX = Directions()
 
 class Location:
-    def __init__(self, latitude, longitude):
-        self.lat = latitude
-        self.lon = longitude
+    def __init__(self, latitude, longitude, name=None, distance=None):
+        self.name = name
+        self.distance = distance
+        self.latitude = latitude
+        self.longitude = longitude
 
-    def __repr__(self):
-        return self.lat + ' ' + self.lon
+#    def __repr__(self):
+#        return '{0} {1}'.format(self.latitude, self.longitude)
 
-    def get_coord_list(self):
-        return [self.lat, self.lon]
+    def get_coord_set(self):
+        return (self.latitude, self.longitude)
 
 
 def get_location(address):
     ''' Returns coordinates for given address. '''
 
-    geolocator = Nominatim()
-    location = geolocator.geocode(address)
+    location = Nominatim().geocode(address)
     return Location(location.latitude, location.longitude)
 
-def get_distance(location_a, location_b):
-    ''' Returns distance in meters and seconds between given points. '''
+def get_distance_meters(location_a_coords, location_b_coords):
+    ''' Returns distance in meters between given points. '''
 
-    distance_meters = int(vincenty(location_a.getCoordList,
-                          location_b.getCoordList).meters)
-    distance_seconds = int(distance_meters)/1.4
+    distance_meters = int(vincenty(location_a_coords,
+                                   location_b_coords).meters)
+    return distance_meters
+
+def get_distance_seconds(distance_meters):
+    ''' Returns distance in seconds from given distance in meters. '''
+
     # 1.4m/s is the average walking speed of a human
-    return [distance_meters, distance_seconds]
+    distance_seconds = int(distance_meters)/1.4
+    return distance_seconds
 
 def get_first_route(frm, to):
     ''' Returns first available route. '''
@@ -42,7 +48,7 @@ def get_first_route(frm, to):
     routes = imhdsk.routes(frm, to, time='', date='')
     if len(routes) > 0:
         return routes[0]
-    print 'No route found!'
+    print('No route found!')
 
 def get_routes(frm, to):
     ''' Returns all available routes. '''
@@ -50,30 +56,31 @@ def get_routes(frm, to):
     routes = imhdsk.routes(frm, to, time='', date='')
     if len(routes) > 0:
         return routes
-    print 'No routes found!'
+    print('No routes found!')
 
 def get_nearest_stop(location):
     ''' Returns nearest stop to the provided location. '''
 
-    loc_coords = location.getCoordList()
-    for t_stop in trams:
-        #
+    stops = {}
+    for name, coords in trams.items():
+        distance = get_distance_meters(location.get_coord_set(), coords)
+        stops.update({name: distance})
+
+    nearest = min(stops.values())
+    for name, distance in stops.items():
+        if distance == nearest:
+            print(name + ' ' + str(distance))
 
 def get_tram_stop_location(name):
     ''' Returns latitude and longitude of tram stop called <name>. '''
 
     if name in trams:
-       return trams[name]
+        return trams[name]
     print('Tram stop is not in database.')
 
-def get_tram_stop_name(latitude, longitude):
+def get_tram_stop_name(lat_long):
     ''' Returns name of tram stop with <lat> and <long> coordinates. '''
 
-    try:
-        stop_name = trams.keys()[trams.values().index([latitude, longitude])]
-        return stop_name
-    except ValueError:
-        print('Cannot retrieve stop name from database.')
-
-
-getStopLocation('Aupark')
+    if lat_long in trams_rev:
+        return trams_rev[lat_long]
+    print('Tram with given latitude and longitude is not in database.')
