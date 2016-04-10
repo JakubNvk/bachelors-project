4f0f5b5a7fb74add3bed271d1f370747d5943cfe
Jakub Novak <jakub.novak@gmail.com>
2016-04-10 18:06:20 +0200

    added find_route
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- bcf.py
+++ bcf.py
@@ -1,5 +1,7 @@
 # -*- coding: UTF-8 -*-
+
 import imhdsk
+import unicodedata
 from trams import trams, trams_rev
 from mapbox import Directions
 from geopy.geocoders import Nominatim
@@ -9,18 +11,23 @@ from geopy.distance import vincenty
 MAPBOX = Directions()
 
 class Location:
-    def __init__(self, latitude, longitude, name=None, distance=None):
-        self.name = name
-        self.distance = distance
+    def __init__(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
-
-#    def __repr__(self):
-#        return '{0} {1}'.format(self.latitude, self.longitude)
 
     def get_coord_set(self):
         return (self.latitude, self.longitude)
 
+def normalize(stop):
+    ''' Removes special characters from given stop. '''
+
+    stop = unicodedata.normalize('NFKD', stop)
+    normalized = ''
+
+    for letter in stop:
+        if not unicodedata.combining(letter):
+            normalized += letter
+    return normalized
 
 def get_location(address):
     ''' Returns coordinates for given address. '''
@@ -42,21 +49,24 @@ def get_distance_seconds(distance_meters):
     distance_seconds = int(distance_meters)/1.4
     return distance_seconds
 
-def get_first_route(frm, to):
+def get_first_mhd(frm, to):
     ''' Returns first available route. '''
 
     routes = imhdsk.routes(frm, to, time='', date='')
-    if len(routes) > 0:
-        return routes[0]
-    print('No route found!')
+    if len(routes) == 0:
+        print('No route found!')
+        return
+    return routes[0]
 
-def get_routes(frm, to):
+
+def get_mhd(frm, to):
     ''' Returns all available routes. '''
 
     routes = imhdsk.routes(frm, to, time='', date='')
-    if len(routes) > 0:
-        return routes
-    print('No routes found!')
+    if len(routes) == 0:
+        print('No routes found!')
+        return
+    return routes
 
 def get_nearest_stop(location):
     ''' Returns nearest stop to the provided location. '''
@@ -69,7 +79,7 @@ def get_nearest_stop(location):
     nearest = min(stops.values())
     for name, distance in stops.items():
         if distance == nearest:
-            print(name + ' ' + str(distance))
+            return name
 
 def get_tram_stop_location(name):
     ''' Returns latitude and longitude of tram stop called <name>. '''
@@ -84,3 +94,18 @@ def get_tram_stop_name(lat_long):
     if lat_long in trams_rev:
         return trams_rev[lat_long]
     print('Tram with given latitude and longitude is not in database.')
+
+def find_route(frm, to):
+    ''' Finds route from location A to location B. '''
+
+    if frm == to:
+        print('Location from and to are the same.')
+        return
+
+    loc_a = get_location(frm)
+    loc_b = get_location(to)
+    stop_a = get_nearest_stop(loc_a)
+    stop_b = get_nearest_stop(loc_b)
+    print(get_first_mhd(normalize(stop_a), normalize(stop_b)))
+
+find_route('Ružinovská 2747', 'Kollárovo námestie')
